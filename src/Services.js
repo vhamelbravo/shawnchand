614077033e3c2dd79b8ff526372329873281b11d
vhamelbravo <vicentehamelbravo@gmail.com>
2023-05-10 20:22:18 -0400

    commit

--- src/Services.js
+++ src/Services.js
@@ -1,6 +1,15 @@
+import Navbar from './Navbar'
+import Footer from './Footer'
 function Services () {
 return (
-<h1> Test </h1>
+	<>
+	<div className="h-screen">
+<h1 className="text-slate-400 text-9xl translate-x-[32%]"> Services </h1>
+	</div>
+	<div className="my-[-40%]">
+	<Navbar />
+	</div>
+</>	
 )
 }
 
