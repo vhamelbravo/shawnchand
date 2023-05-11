a2bb4d52178c9241c7cbc944ae6b952d4f9d4208
vhamelbravo <vicentehamelbravo@gmail.com>
2023-05-10 22:04:17 -0400

    commit

--- src/About.js
+++ src/About.js
@@ -8,15 +8,19 @@ return (
 	
 	<div className="flex flex-col">
 <h1 className="text-slate-400 text-9xl translate-x-[30%] "> About Me </h1>
-	<div className="border border-slate-400 w-[60rem] mx-[18%] my-[3%] h-[550px]">
+	<div className="overflow-y-scroll border border-slate-400 w-[60rem] mx-[18%] my-[3%] h-[500px]  ">
 	<img className="scale-[30%] translate-y-[-32%] translate-x-[-30%]" src={image} />
-	<p className="text-white max-w-[500px] translate-y-[-195%] translate-x-[80%]"> n consectetur nunc pharetra ornare scelerisque. Vivamus pellentesque, dui et faucibus hendrerit, felis quam efficitur erat, sed commodo dolor elit vel augue. Suspendisse elementum in ante eget tincidunt. Nulla mollis, libero ac interdum posuere, diam tellus egestas ipsum, eu rhoncus enim arcu ac tellus. Mauris a lacus a diam molestie pharetra. Nam luctus mattis egestas. Nullam viverra commodo urna, eget vulputate augue faucibus eget. Donec consequat nibh aliquet posuere sodales. Quisque ut purus id dui faucibus lobortis. Donec mi ipsum, vehicula at nunc ac, congue accumsan ipsum. Maecenas ut tristique lectus. Quisque luctus placerat felis, et imperdiet tortor ultrices ut. Aenean luctus at diam vitae elementum. Phasellus mi metus, dictum eget felis sed, porta fringilla magna. Integer pharetra egestas lacus, tincidunt tincidunt dui facilisis ut.  </p>
-	
+	<p className="text-white max-w-[500px] h-[360px] translate-y-[-260%] translate-x-[80%]"> With expertise spanning digital and in-person consultancy, design, project management, and technical services, I offer a diverse range of creative solutions. Whether you need assistance with a project or require technical guidance, I've got you covered.
+
+	Given my online presence, I'm readily available to connect. Feel free to reach out to me through my business Discord or by using the Contact page on this website. Let's explore how I can help you bring your ideas to life!
+
+	To access my services, simply head over to the contact page, where we can arrange a time to discuss your needs in detail. I look forward to collaborating with you! 
+</p>	
 	</div>
 	
 	
 	<div className="relative">
-	<div className="translate-y-[-25%]">
+	<div className="translate-y-[-15%]">
         <Navbar />	
 	</div>
 	</div>
@@ -29,16 +33,16 @@ return (
 	<div className="border border-slate-400 w-[50rem] h-[200px] p-[.3%] mx-[50%] my-[5%]">
 	<p className="text-white"> n consectetur nunc pharetra ornare scelerisque. Vivamus pellentesque, dui et faucibus hendrerit, felis quam efficitur erat, sed commodo dolor elit vel augue. Suspendisse elementum in ante eget tincidunt. Nulla mollis, libero ac interdum posuere, diam tellus egestas ipsum, eu rhoncus enim arcu ac tellus. Mauris a lacus a diam molestie pharetra. Nam luctus mattis egestas. Nullam viverra commodo urna, eget vulputate augue faucibus eget. Donec consequat nibh aliquet posuere sodales. Quisque ut purus id dui faucibus lobortis. Donec mi ipsum, vehicula at nunc ac, congue. </p>
 	</div>
-<h1 className="text-slate-400 text-5xl translate-x-[13%] translate-y-[-180%] "> Section 3 </h1>
-	<div className="border border-slate-400 w-[50rem] h-[200px] p-[.3%]">
+<h1 className="text-slate-400 text-5xl translate-x-[13%] translate-y-[-180%] mx-[10%] "> Section 3 </h1>
+	<div className="border border-slate-400 w-[50rem] h-[200px] p-[.3%] mx-[10%]">
 	<p className="text-white"> n consectetur nunc pharetra ornare scelerisque. Vivamus pellentesque, dui et faucibus hendrerit, felis quam efficitur erat, sed commodo dolor elit vel augue. Suspendisse elementum in ante eget tincidunt. Nulla mollis, libero ac interdum posuere, diam tellus egestas ipsum, eu rhoncus enim arcu ac tellus. Mauris a lacus a diam molestie pharetra. Nam luctus mattis egestas. Nullam viverra commodo urna, eget vulputate augue faucibus eget. Donec consequat nibh aliquet posuere sodales. Quisque ut purus id dui faucibus lobortis. Donec mi ipsum, vehicula at nunc ac, congue. </p>
 	</div>
 <h1 className="text-slate-400 text-5xl translate-x-[63%]"> Section 4 </h1>
 	<div className="border border-slate-400 w-[50rem] h-[200px] p-[.3%] mx-[50%] my-[5%]">
 	<p className="text-white"> n consectetur nunc pharetra ornare scelerisque. Vivamus pellentesque, dui et faucibus hendrerit, felis quam efficitur erat, sed commodo dolor elit vel augue. Suspendisse elementum in ante eget tincidunt. Nulla mollis, libero ac interdum posuere, diam tellus egestas ipsum, eu rhoncus enim arcu ac tellus. Mauris a lacus a diam molestie pharetra. Nam luctus mattis egestas. Nullam viverra commodo urna, eget vulputate augue faucibus eget. Donec consequat nibh aliquet posuere sodales. Quisque ut purus id dui faucibus lobortis. Donec mi ipsum, vehicula at nunc ac, congue. </p>
 	</div>
-<h1 className="text-slate-400 text-5xl translate-x-[13%] translate-y-[-180%]"> Section 5 </h1>
-	<div className="border border-slate-400 w-[50rem] h-[200px] p-[.3%]">
+<h1 className="text-slate-400 text-5xl translate-x-[13%] translate-y-[-180%] mx-[10%]"> Section 5 </h1>
+	<div className="border border-slate-400 w-[50rem] h-[200px] p-[.3%] mx-[10%]">
 	<p className="text-white"> n consectetur nunc pharetra ornare scelerisque. Vivamus pellentesque, dui et faucibus hendrerit, felis quam efficitur erat, sed commodo dolor elit vel augue. Suspendisse elementum in ante eget tincidunt. Nulla mollis, libero ac interdum posuere, diam tellus egestas ipsum, eu rhoncus enim arcu ac tellus. Mauris a lacus a diam molestie pharetra. Nam luctus mattis egestas. Nullam viverra commodo urna, eget vulputate augue faucibus eget. Donec consequat nibh aliquet posuere sodales. Quisque ut purus id dui faucibus lobortis. Donec mi ipsum, vehicula at nunc ac, congue. </p>
 	</div>
 <h1 className="text-slate-400 text-5xl translate-x-[63%]"> Section 6 </h1>
--- src/Profile.js
+++ src/Profile.js
@@ -2,19 +2,23 @@ import image from './assets/person.png'
 function Profile () {
 return (
 	<>
-	<div className="profile border border-slate-400 w-[50rem] pb-[10rem] mt-[-40%] translate-x-[55%] ">
+	<div className="profile border border-slate-400 w-[50rem] pb-[11rem] mt-[-40%] translate-x-[55%] ">
 	<h1 className="text-white text-4xl translate-x-[22%]"> Welcome to My Forest! </h1>
-	<p className="relative text-white translate-x-[23%] translate-y-[10%] ">  Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /> Maecenas mollis porta ex, ut  vulputate urna aliquet a. <br />  Nunc egestas arcu at elit finibus auctor. Integer ac <br/> ultricies  est. Donec egestas gravida ante, et fringilla dui <br />  elementum at. Donec condimentum diam congue  ligula <br/> condimentum, non euismod elit volutpat. Duis ut dictum <br/> sem,  nec congue ligula. In maximus  faucibus dolor  a <br/> varius. Quisque efficitur nibh in est laoreet varius.
+	<p className="relative text-white translate-x-[23%] translate-y-[10%] "> Welcome to my creative corner! This website and blog <br/> page  are a delightful blend of various pixel art sites <br /> that have inspired me across the web. <br/>
+
+	As an avid art and design enthusiast, I draw inspiration <br/> from a multitude of styles. If youre curious to learn <br/> more about me and my artistic journey, be sure to visit <br/> the About section.
 </p>
-	<p className="text-white text-3xl translate-y-[180%] translate-x-[5%]"> Quick facts </p>
+	<p className="text-white text-3xl translate-y-[100%] translate-x-[5%]"> Here are some quick facts about me </p>
 	<ul>
-	<li> <p className="text-white text-2xl translate-y-[280%] translate-x-[5%]">-Sample text </p> </li>
-	<li> <p className="text-white text-2xl translate-y-[280%] translate-x-[5%]">-Sample text </p> </li>
-	<li> <p className="text-white text-2xl translate-y-[280%] translate-x-[5%]">-Sample text </p> </li>
+	<li> <p className="text-white translate-y-[150%] translate-x-[5%]">- Proudly hailing from Canada.</p> </li>
+	<li> <p className="text-white translate-y-[150%] translate-x-[5%]">- I harbor a deep affection for adorable koalas.</p> </li>
+	<li> <p className="text-white translate-y-[70%] translate-x-[5%]">- The fall and winter months hold a special <br/> place in my heart.</p>
+	</li>
+	<li> <p className="text-white translate-y-[60%] translate-x-[5%]">- I'm grateful for a successful kidney transplant, which has brought happiness and good health to my life .</p> </li>
 
 	</ul>
-	<img className="z-20 relative scale-[20%] translate-x-[-39%] translate-y-[-75%] " src={image} />
-	<div className="relative border border-slate-400 w-[10rem] pb-[10rem] mt-[-127%] translate-y-[-41%] translate-x-[5%] ">
+	<img className="z-20 relative scale-[20%] translate-x-[-39%] translate-y-[-80%] " src={image} />
+	<div className="relative border border-slate-400 w-[10rem] pb-[10rem] mt-[-132%] translate-y-[-41%] translate-x-[5%] ">
 	</div>
 	</div>
 	</>
